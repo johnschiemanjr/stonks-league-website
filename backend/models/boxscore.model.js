1ca19b0a6de582e01d8d4f7b77449645f187affb
johnschiemanjr <johnschiemanjr@gmail.com>
2021-02-06 00:38:39 -0500

    ran formatter

--- backend/models/boxscore.model.js
+++ backend/models/boxscore.model.js
@@ -4,22 +4,22 @@ const Player = require("./player.model.js").schema;
 const Schema = mongoose.Schema;
 
 const boxscoreSchema = new Schema({
-	homeTeamId: {
-		type: Number,
-		required: true,
-	},
-	awayTeamId: {
-		type: Number,
-		required: true,
-	},
-	homeRoster: {
-		type: [Schema.Types.ObjectId],
-		ref: "Player",
-	},
-	awayRoster: {
-		type: [Schema.Types.ObjectId],
-		ref: "Player",
-	},
+  homeTeamId: {
+    type: Number,
+    required: true,
+  },
+  awayTeamId: {
+    type: Number,
+    required: true,
+  },
+  homeRoster: {
+    type: [Schema.Types.ObjectId],
+    ref: "Player",
+  },
+  awayRoster: {
+    type: [Schema.Types.ObjectId],
+    ref: "Player",
+  },
 });
 
 const Boxscore = mongoose.model("Boxscore", boxscoreSchema);
--- backend/models/week.model.js
+++ backend/models/week.model.js
@@ -4,25 +4,25 @@ const Boxscore = require("./boxscore.model.js");
 const Schema = mongoose.Schema;
 
 const weekSchema = new Schema(
-    {
-        year: {
-            type: Number,
-            required: true,
-            unique: false,
-        },
-        weekId: {
-            type: Number,
-            required: true,
-            unique: false,
-        },
-        boxscores: {
-            type: [Schema.Types.ObjectId],
-            ref: "Boxscore",
-        },
+  {
+    year: {
+      type: Number,
+      required: true,
+      unique: false,
     },
-    {
-        timestamps: true,
-    }
+    weekId: {
+      type: Number,
+      required: true,
+      unique: false,
+    },
+    boxscores: {
+      type: [Schema.Types.ObjectId],
+      ref: "Boxscore",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 weekSchema.index({ year: 1, weekId: 1 }, { unique: true });
