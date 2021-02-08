8353790a3841cced97f07134492305d57b822500
johnschiemanjr <johnschiemanjr@gmail.com>
2021-02-07 22:52:36 -0500

    continued work on overview page

---
+++ src/components/overview-pane.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../stylesheets/owners-list.css";
+
+export default class OverviewPane extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello</h3>
+            </div>
+        );
+    }
+}
--- src/components/owners-list.component.js
+++ src/components/owners-list.component.js
@@ -4,74 +4,61 @@ import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import "../stylesheets/owners-list.css";
 
-const Owner = (props) => (
-  <tr>
-    <td>{props.owner.ownerId}</td>
-    <td>{props.owner.teamName}</td>
-    <td>{props.owner.espnId}</td>
-    <td>{props.owner.ownerName}</td>
-  </tr>
-);
+import OverviewPane from "./overview-pane.component";
 
 export default class OwnersList extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { owners: [], dropdownOption: "Select Owner" };
-  }
+        this.state = { owners: [], dropdownOption: "Owner Overview" };
 
-  componentDidMount() {
-    axios
-      .get("http://localhost:5000/owners/")
-      .then((response) => {
-        this.setState({ owners: response.data });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  ownerList() {
-    return this.state.owners.map((currentOwner) => {
-      return <Owner owner={currentOwner} key={currentOwner._id} />;
-    });
-  }
-
-  ownerDropdownList() {
-    var teamNames = [];
-    for (var i = 0; i < this.state.owners.length; i++) {
-      teamNames.push(this.state.owners[i].teamName);
+        this.onSelect = this.onSelect.bind(this);
     }
-    return teamNames;
-  }
 
-  onSelect(selection) {
-    console.log("hello");
-    console.log(selection);
-  }
+    componentDidMount() {
+        axios
+            .get("http://localhost:5000/owners/")
+            .then((response) => {
+                this.setState({ owners: response.data });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-  render() {
-    return (
-      <div>
-        <h3>John Snowzeliak (PLACEHOLDER)</h3>
-        <Dropdown
-          options={this.ownerDropdownList()}
-          value={this.state.dropdownOption}
-          onChange={this._onSelect}
-          className="ownerDropdown"
-        />
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Team ID</th>
-              <th>Team Name</th>
-              <th>ESPN ID</th>
-              <th>Owner</th>
-            </tr>
-          </thead>
-          <tbody>{this.ownerList()}</tbody>
-        </table>
-      </div>
-    );
-  }
+    ownerDropdownList() {
+        var teamNames = [];
+        for (var i = 0; i < this.state.owners.length; i++) {
+            teamNames.push(this.state.owners[i].teamName);
+        }
+        return teamNames;
+    }
+
+    onSelect(selection) {
+        const owners = this.state.owners.filter((owner) => {
+            return owner.teamName === selection.value;
+        });
+        this.setState({ dropdownOption: owners[0].teamName });
+    }
+
+    render() {
+        return (
+            <div id="parent-div">
+                <div id="ownername-div">
+                    <h3>{this.state.dropdownOption}</h3>
+                </div>
+                <div id="dropdown-div">
+                    <Dropdown
+                        options={this.ownerDropdownList()}
+                        placeholder="Select Owner"
+                        onChange={this.onSelect}
+                        className="ownerDropdown"
+                    />
+                </div>
+                <div id="overview-pane-div">
+                    <OverviewPane />
+                </div>
+            </div>
+        );
+    }
 }
