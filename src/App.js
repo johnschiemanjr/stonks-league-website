bdb6d573033738bc0f93e71072e28dc29c2033f8
johnschiemanjr <johnschiemanjr@gmail.com>
2021-02-11 00:34:54 -0500

    converted owner page to use grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import Navbar from "./components/navbar.component";
 import OwnersList from "./components/owners-list.component";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <br />
-      <Route path="/" exact component={OwnersList} />
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div className="grid-container">
+                <div className="grid-item owners-list">
+                    {" "}
+                    <Route path="/" exact component={OwnersList} />
+                </div>
+                <div className="grid-item owner-pane">Owner Pane</div>
+                <div className="grid-item regular-season">
+                    Regular Season Performance
+                </div>
+                <div className="grid-item postseason">
+                    Postseason Performance
+                </div>
+                <div className="grid-item historic">Historic Performance</div>
+            </div>
+            <div></div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/overview-pane.component.js
+++ src/components/overview-pane.component.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import "../stylesheets/owners-list.css";
 
 export default class OverviewPane extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Hello</h3>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h3>Hello</h3>
+            </div>
+        );
+    }
 }
