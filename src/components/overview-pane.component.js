7c07019756fd6ab15f782479ff1f6c09f0f8406e
johnschiemanjr <johnschiemanjr@gmail.com>
2021-02-19 00:58:00 -0500

    continued work on owner page

--- src/components/overview-pane.component.js
+++ src/components/overview-pane.component.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 
+class InfoCard extends Component {
+    render() {
+        return (
+            <div className="text-center owner-pane-info-card">
+                <div>
+                    <div>
+                        <h5>{this.props.title}</h5>
+                        {this.props.content}
+                        {"\n"}
+                        {this.props.info}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 export default class OverviewPane extends Component {
     render() {
         return (
-            <div className="card">
-                <div className="card-header">{this.props.teamName}</div>
+            <div className="card owner-pane">
+                <div className="card-header owner-pane-header">
+                    {this.props.teamName}
+                </div>
                 <div className="card-body">
-                    <blockquote className="blockquote mb-0">
-                        <p>
-                            A well-known quote, contained in a blockquote
-                            element.
-                        </p>
-                        <footer className="blockquote-footer">
-                            Someone famous in{" "}
-                            <cite title="Source Title">Source Title</cite>
-                        </footer>
-                    </blockquote>
+                    <div className="row">
+                        <div className="col-lg-4 owner-pane-column">
+                            <InfoCard title="Seasons" content={1} />
+                            <InfoCard title="Record" content="6-7-0" />
+                            <InfoCard title="Current Win Streak" content="W1" />
+                            <InfoCard
+                                title="Biggest Winning Streak"
+                                content={4}
+                                info="2016 Week 10 - 2016 Week 5"
+                            />
+                            <InfoCard
+                                title="Biggest Losing Streak"
+                                content={4}
+                            />
+                            <InfoCard title="Seasons" content={1} />
+                        </div>
+                        <div className="col-lg-4">
+                            <InfoCard title="Seasons" content={1} />
+                        </div>
+                        <div className="col-lg-4">
+                            <InfoCard title="Seasons" content={1} />
+                        </div>
+                    </div>
                 </div>
             </div>
         );
