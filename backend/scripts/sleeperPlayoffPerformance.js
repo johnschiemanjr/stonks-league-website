aa92caa2d3b0b18f619e313f36d7abadf63b12cc
johnschiemanjr <johnschiemanjr@gmail.com>
2022-01-08 12:35:07 -0500

    put together playoffperformance for sleeper

--- backend/scripts/sleeperPlayoffPerformance.js
+++ backend/scripts/sleeperPlayoffPerformance.js
@@ -6,16 +6,100 @@ const winnersBracket = require("../../data/" +
     YEAR +
     "/league_winners_bracket.json");
 
+let appearances = [];
 //console.log(winnersBracket);
+
+winnersBracket.forEach((matchup) => {
+    let team1Id = matchup.t1;
+    let team2Id = matchup.t2;
+
+    if (appearances.indexOf(team1Id) === -1) {
+        appearances.push(team1Id);
+    }
+    if (appearances.indexOf(team2Id) === -1) {
+        appearances.push(team2Id);
+    }
+});
+
+// print winners bracket
+
+//console.log(appearances);
 
 let week1Matchups = winnersBracket.filter((matchup) => {
     return matchup.r === 1;
 });
+let week1Winners = [];
+
+week1Matchups.forEach((matchup) => {
+    console.log(matchup.w + " beats " + matchup.l);
+    week1Winners.push(matchup.w);
+});
+
 let week2Matchups = winnersBracket.filter((matchup) => {
     return matchup.r === 2;
 });
+let week2Winners = [];
+
+week2Matchups.forEach((matchup) => {
+    if (
+        week1Winners.indexOf(matchup.w) === -1 &&
+        week1Winners.indexOf(matchup.l) === -1
+    ) {
+        // Do nothing, this is a consolation game
+    } else {
+        console.log(matchup.w + " beats " + matchup.l);
+        week2Winners.push(matchup.w);
+    }
+});
+
 let week3Matchups = winnersBracket.filter((matchup) => {
     return matchup.r === 3;
 });
+let week3Winners = [];
 
-console.log(week1Matchups);
+week3Matchups.forEach((matchup) => {
+    if (
+        week2Winners.indexOf(matchup.w) === -1 &&
+        week2Winners.indexOf(matchup.l) === -1
+    ) {
+        // Do nothing, this is a consolation game
+    } else {
+        console.log(matchup.w + " beats " + matchup.l);
+        week3Winners.push(matchup.w);
+    }
+});
+
+let ownersToUpdate = [];
+appearances.forEach((appearanceId) => {
+    let performance = new Object();
+    performance.wins = 0;
+    performance.losses = 0;
+    performance.ties = 0;
+    performance.appearances = 0;
+    performance.championships = 0;
+    performance.runnerups = 0;
+
+    performance.ownerId = sleeperConverter.rosterIdToUserId(YEAR, appearanceId);
+    performance.appearances++;
+
+    if (week3Winners.indexOf(appearanceId) !== -1) {
+        performance.championships++;
+    } else {
+        if (week2Winners.indexOf(appearanceId) !== -1) {
+            performance.runnerups++;
+        }
+        performance.losses++;
+    }
+
+    if (week1Winners.indexOf(appearanceId) !== -1) {
+        performance.wins++;
+    }
+    if (week2Winners.indexOf(appearanceId) !== -1) {
+        performance.wins++;
+    }
+    if (week3Winners.indexOf(appearanceId) !== -1) {
+        performance.wins++;
+    }
+
+    console.log(performance);
+});
